8a5c7c8838a33722a36cfdc1e95ff5f0d8f6c384
Paul Wishart <misterwishart@gmail.com>
2018-11-26 18:44:29 +0000

    Slight tidying up of code

--- src/models/prime_checker.js
+++ src/models/prime_checker.js
@@ -8,26 +8,29 @@ const PrimeChecker = function () {
 PrimeChecker.prototype.numberIsPrime = function (number) {
 
   if (number === 1) {return false;}
-
   if (number <= this.rangeChecked) {return this.isInArray(number);}
 
-  for (let i = this.rangeChecked+1; i <= Math.sqrt(number); i++) {
+  let lowerLimit = this.rangeChecked+1;
+  let upperLimit = Math.round(Math.sqrt(number));
 
-    if (this.isNewPrime(i)) {
-      this.primeArray.push(i);
-      this.rangeChecked = i;
-    }
-    if (number % i === 0) {
-      return false;
-    }
+  for (let i = lowerLimit; i <= upperLimit; i++) {
+
+    if (this.isNewPrime(i)) {this.appendArray(i);}
+    this.rangeChecked = i;
+    if (number % i === 0) {return false;}
   }
 
   return true;
 };
 
+PrimeChecker.prototype.appendArray = function (number) {
+  this.primeArray.push(number);
+};
+
 PrimeChecker.prototype.isInArray = function (number) {
   return this.primeArray.includes(number)
 };
+
 
 PrimeChecker.prototype.isNewPrime = function (number) {
 
